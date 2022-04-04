71ed5f1a2b178b68a7a7d2ab75a25cd3404db21a
Kyra Christensen <christensen.kyral@gmail.com>
2022-04-04 14:22:50 -0700

    uniqueChar challenge is comeplete

--- dsna.js
+++ dsna.js
@@ -42,5 +42,20 @@ function anagrams(wordOne, wordTwo) {
 
 console.log(anagrams('superintended', 'unpredestined'));
 console.log(anagrams('pictorialness', 'documentarily'));
-//unique-char
 
+
+//unique-char
+function uniqueChar(string) {
+  const chars = string.split('').sort();
+  for (let i = 0; i < chars.length; i++) {
+    const current = chars[i];
+    const prev = chars[i - 1];
+    const next = chars[i + 1];
+    if (current != prev && current != next) return current;
+  }
+  
+  return '_';
+}
+console.log(uniqueChar('abdacabad'));
+console.log(uniqueChar('abacabaabacaba'));
+console.log(uniqueChar('abacabad'));
